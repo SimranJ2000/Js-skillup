c6ec21d039a92822e11c03c7f9189c967a4bb112
some_name <simran.jahan@capgemini.com>
2025-08-29 23:39:44 +0530

    For loop questions

---
+++ loop.js
@@ -0,0 +1,13 @@
+//loops are used to execute a piece of code multiple times until a certain condition is met.
+//There are different types of loops in JavaScript, including for, while, and do...while loops. 
+//Here are some examples of each type of loop:  
+//1. For Loop
+for(let count = 1; count <= 5; count++){
+    console.log("Apna Ghar");
+}
+//calculate the sum of 1 to 5
+let sum = 0;
+for(let i = 1; i <= 5; i++){
+    sum= sum+i; // sum = sum + i
+}
+console.log("The sum of 1 to 5 is: " + sum);
